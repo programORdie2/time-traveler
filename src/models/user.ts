f10c0c55bfb216e877a600ec33828d4b783d7bc5
programORdie <contact.programordie@gmail.com>
2024-10-03 00:27:41 +0200

    small db update

--- src/models/user.ts
+++ src/models/user.ts
@@ -9,10 +9,6 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    },
-    capsuleIds: {
-        type: [String],
-        default: []
     }
 });
 
