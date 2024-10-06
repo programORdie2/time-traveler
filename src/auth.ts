8dc6b755a50c71639bf31f1922db45b7e797ac4c
programORdie <contact.programordie@gmail.com>
2024-10-06 10:44:41 +0200

    optimized auth for build

--- src/auth.ts
+++ src/auth.ts
@@ -50,4 +50,38 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             },
         }),
     ],
+
+    trustHost: true,
+    cookies: {
+        sessionToken: {
+            name: "sessionToken",
+            options: {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: "lax",
+                path: "/",
+                maxAge: 60 * 60 * 24 * 30,
+            },
+        },
+        callbackUrl: {
+            name: "_callbackUrl",
+            options: {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: "lax",
+                path: "/",
+                maxAge: 60 * 60 * 24 * 30,
+            },
+        },
+        csrfToken: {
+            name: "_csrfToken",
+            options: {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: "lax",
+                path: "/",
+                maxAge: 60 * 60 * 24 * 30,
+            },
+        },
+    },
 })
