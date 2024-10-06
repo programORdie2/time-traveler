a06e4fc4305cf15d19e4b2fd434a56f3c840d04d
programORdie <contact.programordie@gmail.com>
2024-10-06 19:21:39 +0200

    fix calendar

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -60,8 +60,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        PreviousMonthButton: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" />,
-        NextMonthButton: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" />,
+        IconLeft: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" />,
+        IconRight: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" />,
       }}
       {...props}
     />
